ced528ffa096c87b0ab19b4310cc268e4f4768ae
GGWP-grats <grats420@gmail.com>
2021-08-16 16:57:48 +0300

    check high performance work

---
+++ go.mod
@@ -0,0 +1,5 @@
+module wp-iss
+
+go 1.16
+
+require github.com/valyala/fasthttp v1.28.0
--- scan.go
+++ scan.go
@@ -5,59 +5,100 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/valyala/fasthttp"
 )
 
-var banner = `
-    _    _ ______       _____  _____  _____ 
-   | |  | || ___ \     |_   _|/  ___|/  ___|
-   | |  | || |_/ /______ | |  \ '--. \ '--. 
-   | |/\| ||  __/|______|| |   '--. \ '--. \
-   \  /\  /| |          _| |_ /\__/ //\__/ /
-    \/  \/ \_|          \___/ \____/ \____/ 
-                                            
-   [+] WP Install and Setup Scanner
-   [+] Recoded By TiGER HeX [+] We Are TiGER HeX`
-var usage = "USAGE:\tscan [--jobs jobsnum] FILE "
+const (
+	banner = `
+	 _    _ ______       _____  _____  _____ 
+	| |  | || ___ \     |_   _|/  ___|/  ___|
+	| |  | || |_/ /______ | |  \ '--. \ '--. 
+	| |/\| ||  __/|______|| |   '--. \ '--. \
+	\  /\  /| |          _| |_ /\__/ //\__/ /
+	 \/  \/ \_|          \___/ \____/ \____/ 
+												
+	   [+] WP Install and Setup Scanner
+	   [+] Recoded By TiGER HeX [+] We Are TiGER HeX
 
-var jobs int
-var wg sync.WaitGroup
+	   `
+	usage = "USAGE:\tscan [--jobs jobsnum] FILE "
+
+	maxRedirects = 20
+	maxBodySize  = 15000
+)
+
+var (
+	jobs int
+	wg   sync.WaitGroup
+
+	logger *log.Logger
+
+	client = &fasthttp.Client{
+		NoDefaultUserAgentHeader: true, // Don't send: User-Agent: fasthttp
+		MaxConnsPerHost:          20,
+		MaxConnDuration:          10 * time.Second,
+		MaxIdleConnDuration:      10 * time.Second,
+		ReadTimeout:              10 * time.Second,
+		WriteTimeout:             10 * time.Second,
+	}
+)
 
 /*-------------------------GOROUTINE'S ENTRYPOINT----------------------------*/
-func scan(urls chan string, installWriter, setupWriter io.Writer) {
+func scanner(urls chan string, installWriter, setupWriter io.Writer) {
 
-	tr := &http.Transport{
-		IdleConnTimeout:    5 * time.Second,
-		DisableCompression: true,
-	}
-	client := &http.Client{Transport: tr}
+	res := fasthttp.AcquireResponse()
+	req := fasthttp.AcquireRequest()
+
 	defer wg.Done()
+	defer fasthttp.ReleaseResponse(res)
+	defer fasthttp.ReleaseRequest(req)
+
+	req.SetConnectionClose()
+
+	bodyBuff := make([]byte, maxBodySize)
 
 	for url := range urls {
-		resp, err := client.Get(url)
+		req.SetRequestURI(url)
+		err := client.DoRedirects(req, res, maxRedirects)
+
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println(" [-] Bad Response ===>", url)
+			logger.Printf("%v ===> %s\n", err.Error(), url)
 			continue
 		}
-		defer resp.Body.Close()
-		b, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err.Error())
-			resp.Body.Close()
+
+		code := res.StatusCode()
+		bodySize := len(res.Body())
+
+		if code != 200 || bodySize > maxBodySize {
+			fmt.Println(" [-] Failed ===>", url, code)
+			logger.Printf("Bad code(%d )or bodyLen(%d) ===> %s\n", code, bodySize, url)
 			continue
 		}
-		if strings.Contains(string(b), "WordPress &rsaquo; Installation") {
+
+		//Save body from erase to body buffer
+		copy(bodyBuff, res.Body())
+
+		//Check this body contain pattern
+		if strings.Contains(string(bodyBuff), "WordPress &rsaquo; Installation") {
 			fmt.Fprintln(installWriter, url)
-			fmt.Println("\t[+] Install ===>", url)
-		} else if strings.Contains(string(b), "WordPress &rsaquo; Setup Configuration File") {
+			fmt.Println("\t[+] Install ===>", url, code, bodySize)
+		} else if strings.Contains(string(bodyBuff), "WordPress &rsaquo; Setup Configuration File") {
 			fmt.Fprintln(setupWriter, url)
-			fmt.Println("\t[+] Setup ===>", url)
+			fmt.Println("\t[+] Setup ===>", url, code, bodySize)
 		} else {
-			fmt.Println(" [+] Failed ===>", url)
+			if strings.Contains(url, "myfallretreat.conwaybcm.com/newsite") {
+				fmt.Println("DEBUG ", bodyBuff)
+			}
+			fmt.Println(" [-] Failed ===>", url, code)
 		}
 	}
 }
@@ -65,7 +106,11 @@ func scan(urls chan string, installWriter, setupWriter io.Writer) {
 func main() {
 
 	fmt.Println(banner)
-	start := time.Now()
+	fmt.Println("jobs:", jobs)
+
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 
 	/*---------------------------------FILEWORK---------------------------------------*/
 
@@ -79,51 +124,57 @@ func main() {
 	domainReader := bufio.NewReader(f)
 
 	//Creating install.txt and initializing writer on it
+
 	fi, err := os.OpenFile("install.txt", os.O_CREATE+os.O_APPEND+os.O_WRONLY, 0660)
 	if err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 	defer fi.Close()
 
 	//Creating setup.txt and initializing writer on it
+
 	fs, err := os.OpenFile("setup.txt", os.O_CREATE+os.O_APPEND+os.O_WRONLY, 0660)
 	if err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 	defer fs.Close()
-	/*--------------------------------------------------------------------------------*/
 
+	/*--------------------------------------------------------------------------------*/
 	/*--------------------------------START GOROUTINES--------------------------------*/
-	url_chan := make(chan string)
+
+	start := time.Now()
+
+	url_chan := make(chan string, jobs)
 	for i := 0; i < jobs; i++ {
 		wg.Add(1)
-		go scan(url_chan, fi, fs)
+		go scanner(url_chan, fi, fs)
 	}
 	/*--------------------------------------------------------------------------------*/
+	/*----------------------------READ & SEND URL+PATH TO THEM------------------------*/
 
-	/*----------------------------SEND URLS+PATH TO THEM------------------------------*/
-
-	paths := []string{"/", "/wordpress/", "/wp/", "/blog/", "/new/", "/old/", "/newsite/",
-		"/test/", "/dev/", "/New/", "/Wp/", "/Wordpress/", "/Blog/", "/Newsite/", "/Dev/", "Test/", "Old/"}
+	paths := []string{"/", "wordpress/", "/wp", "/blog", "/new", "/old", "/newsite", "/test", "/main", "/cms", "/dev", "backup/"}
 
 	for {
 		line, _, err := domainReader.ReadLine()
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			panic("while reading domain list file")
+			fmt.Println("while reading domain list file")
+			logger.Println(err)
+			return
 		}
+
 		for _, path := range paths {
 			url_chan <- "http://" + string(line) + path
 		}
 	}
 	/*--------------------------------------------------------------------------------*/
-
-	/*--------------------------------CLOSE CONNECTION--------------------------------*/
+	/*-------------------------------- 		END 	  --------------------------------*/
 	close(url_chan)
 	wg.Wait()
 	fmt.Println("Time", time.Now().Sub(start))
-	return
 }
 
 func init() {
@@ -133,5 +184,11 @@ func init() {
 	if len(flag.Args()) < 1 {
 		fmt.Println(usage)
 		os.Exit(2)
+	}
+	l, err := os.OpenFile("error.log", os.O_WRONLY+os.O_CREATE+os.O_TRUNC, 0660)
+	if err != nil {
+		logger = log.Default()
+	} else {
+		logger = log.New(l, "", 0)
 	}
 }
